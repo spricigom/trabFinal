71d0972efbfe17cc4f78843a88d0bfe9093d5ab3
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-12-17 15:19:31 -0300

    estilo do filme e informações adicionais

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
--- src/views/MovieDetailsView.vue
+++ src/views/MovieDetailsView.vue
@@ -16,39 +16,92 @@ onMounted(async () => {
 </script>
 
 <template>
-
     <div class="main">
-        <div class="content">
-            <img :src="`https://image.tmdb.org/t/p/w185${movieStore.currentMovie.poster_path}`"
+        <div class="gradiente"></div>
+        <div class="background">
+            <img :src="`https://image.tmdb.org/t/p/original${movieStore.currentMovie.backdrop_path}`"
                 :alt="movieStore.currentMovie.title" />
+        </div>
 
+        <div class="content">
             <div class="details">
-                <h1>Filme: {{ movieStore.currentMovie.title }}</h1>
-                <p>{{ movieStore.currentMovie.tagline }}</p>
-                <p>{{ movieStore.currentMovie.overview }}</p>
-                <p>Orçamento: ${{ movieStore.currentMovie.budget }}</p>
-                <p>Avaliação: {{ movieStore.currentMovie.vote_average }}</p>
+                <h1>{{ movieStore.currentMovie.title }}</h1>
+                <p><strong>Data de Lançamento:</strong> {{ movieStore.currentMovie.release_date }}</p>
+                <p><strong>Duração:</strong> {{ movieStore.currentMovie.runtime }} minutos</p>
+                <p><strong>Receita:</strong> ${{ movieStore.currentMovie.revenue }}</p>
+                <p><strong>País(es) de Produção:</strong> 
+   <span v-for="country in movieStore.currentMovie.production_countries" :key="country.iso_3166_1">
+       {{ country.name }}<span v-if="!$last">, </span>
+   </span>
+</p>
+<p><strong>Classificação:</strong> {{ movieStore.currentMovie.certification || 'N/A' }}</p>
+<p>
+   <a :href="`https://www.youtube.com/watch?v=${movieStore.currentMovie.videos.results[0]?.key}`" target="_blank">
+      Assistir ao Trailer
+   </a>
+</p>
+
+
+
+
+
+
+                <!-- 
+                <p class="tagline">{{ movieStore.currentMovie.tagline }}</p>
+                <p class="overview">{{ movieStore.currentMovie.overview }}</p>
+                <p><strong>Orçamento:</strong> ${{ movieStore.currentMovie.budget }}</p>
+                <p><strong>Avaliação:</strong> {{ movieStore.currentMovie.vote_average }}</p>
+                -->
             </div>
         </div>
     </div>
-
-    <p>Produtoras</p>
-    <div class="companies">
-        <template v-for="company in movieStore.currentMovie.production_companies" :key="company.id">
-            <img v-if="company.logo_path" :src="`https://image.tmdb.org/t/p/w92${company.logo_path}`"
-                :alt="company.name" />
-            <p v-else>{{ company.name }}</p>
-        </template>
-    </div>
-
 </template>
 
 <style scoped>
-.companies {
+.content {
+    margin-top: 25vh;
     display: flex;
-    flex-direction: row;
-    column-gap: 3rem;
-    align-items: center;
-    margin-bottom: 2rem;
+    width: 45vw;
+}
+
+.details h1 {
+    font-family: 'Quicksand', sans-serif;
+    font-size: 3em;
+}
+
+.main {
+    position: relative;
+    width: auto;
+    height: 100vh;
+    overflow: hidden;
+    color: white;
+    font-family: Arial, sans-serif;
+    padding-left: 10vw;
+}
+
+.background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+    overflow: hidden;
+
+}
+
+.background img {
+    filter: brightness(0.5);
+}
+
+.gradiente {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgb(0, 0, 0);
+    background: linear-gradient(90deg, rgb(0, 0, 0) 0%, rgba(0, 0, 0, 0.76) 18%, rgba(0, 0, 0, 0) 60%, rgba(0, 212, 255, 0) 100%);
+    z-index: -1;
 }
 </style>
