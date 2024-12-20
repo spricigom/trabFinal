1d0dcb05c1ba2b45d0f4eb02b21c6fd8b2d73af7
spricigom <emilispricigo.ifc@gmail.com>
2024-12-20 13:16:33 -0300

    hearder e home prontos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <header>
+    <router-link id="home" to="/"><h1>Lumiplay</h1></router-link>
     <nav>
-      <router-link to="/">Home</router-link>
+      
       <router-link to="/filmes">Filmes</router-link>
       <router-link to="/tv">Programas de TV</router-link>
     </nav>
@@ -11,24 +12,35 @@
   </main>
 </template>
 <style scoped>
+h1{
+  margin-bottom: 0;
+  font-size: 2rem;
+}
 header {
-  height: 3rem;
   display: flex;
+  flex-direction: row;
+  align-items: center ;
+  height: 4rem;
   background-color: black;
   color: #fff;
-  font-size: 1.2rem;
+  font-size: 1.1rem;
+  font-family: "Outfit", serif;
   padding-left: 2rem;
 }
-
 nav {
   column-gap: 2rem;
   margin-bottom: 0;
+  margin-left: 2rem;
   display: flex;
   align-items: center;
 }
-
 nav a {
   text-decoration: none;
   color: #fff;
+}
+#home{
+  text-decoration: none;
+  color: #fff;
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,95 @@
+<script setup>
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const usuario = reactive({
+  email: '',
+  senha: ''
+})
+
+function enviarLogin() {
+  if (usuario.email == '') {
+    alert("Faltou o email")
+    error = true
+  }
+  if (usuario.senha == '') {
+    alert("Faltou a senha")
+    error = true
+  }
+  else {
+    router.push({ path: '/filmes' });
+  }
+}
+</script>
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="teste">
+    <form @submit.prevent="enviarLogin">
+      <h1>Faça seu Login</h1>
+      <div class="caixas">
+      <input  type="email" placeholder="Digite seu Email" v-model="usuario.email">
+      <input type="password" placeholder="Digite sua senha" v-model="usuario.senha">
+    </div>
+      <button type="submit" class="btn ">Concluido</button>
+    </form>
   </div>
 </template>
+<style scoped>
+h1{
+  color: #fff;
+  font-family: "Outfit", serif;
+  margin-top: 30px;
+
+}
+form {
+  display: flex;
+  align-items: center;
+  flex-direction: column; 
+  width: 400px;
+  height: 350px;
+  background-color: black;
+  border-radius: 10px;
+}
+.caixas{
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: auto;
+}
+input{
+  font-size: 2.2vh;
+  font-family: "Outfit", serif;
+  width: 100%;
+  height: 5vh;
+  border-radius: 7px;
+  background-color: rgb(231, 229, 229);
+  border: .1px solid;
+  margin-top: 20px;
+  outline: none;
+  padding: 7px;
+}
+button{
+  margin-top: 5px; 
+  margin-left: 40px; 
+  margin-right: 40px; 
+  margin-bottom: 50px; 
+  width:45% ;
+  height: 4.5vh;
+  border-radius: 7.5px;
+  font-family: "Outfit", serif;
+  background-color: rgb(231, 229, 229);
+  border: none;
+  color: rgb(73, 73, 73);
+
+}
+.teste{
+  height: 91.5vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background: linear-gradient( black,#3e4344);
+}
+</style>
